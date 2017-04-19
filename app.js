6e69fa802ccc31d95612f26e5a6c966c4051fa7c
Michael Barritt <michaelbarritt@hotmail.com>
2017-04-19 08:11:22 +0000

    add mLab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes     = require("./routes/comments"),
     campgroundRoutes  = require("./routes/campgrounds"),
     indexRoutes       = require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_camp_v12");
+// mongoose.connect("mongodb://localhost/yelp_camp_v12");
+mongoose.connect("mongodb://butchinator:YelpCamp!123@ds163940.mlab.com:63940/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
